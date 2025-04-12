1ee383a1133b71c2886817509881fd14ff7c4d05
VilaMariano <vilam@udesa.edu.ar>
2025-04-12 11:55:48 -0300

    Punto 8, pagina de not found finaliza

--- piprog3/src/App.js
+++ piprog3/src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter, Link, Route, Switch} from 'react-router-dom'
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home";
 import PeliculasPopulares from "./pages/PeliculasPopulares";
+import NotFound from "./components/NotFound/NotFound";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/populares" component={PeliculasPopulares} />
+      <Route component={NotFound} />
       </Switch>
       <Footer />
     </BrowserRouter>
---
+++ piprog3/src/components/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./NotFound.css"
+
+const NotFound = () => {
+    return (
+        <div ClassName = "NotFoundDiv">
+            <h2 className = "NotFound">404 Not Found</h2>
+            <p>La página no existe.</p>
+            <a href="/" className="NotFoundButton">Volver al inicio</a>
+        </div>
+    );
+};
+
+export default NotFound;
